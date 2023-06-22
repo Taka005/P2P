112e98c6b82b31a27d0efe294dccf9f01f746d07
Taka005 <01taka005@gmail.com>
2023-06-22 23:18:28 +0900

    add

--- p2p.js
+++ p2p.js
@@ -1,16 +1,22 @@
 const net = require("net");
+const crypto = require("crypto");
+const { EventEmitter } = require("events");
+const package = require("./package.json");
 
-class P2P{
+class P2P extends EventEmitter{
   constructor(config){
+    super();
+
     this.config = {
       ServerPort: config.ServerPort||5000,
       ClientPort: config.ClientPort||5000,
-      ket: config.key||"NONE"
+      key: config.key||"NONE",
+      UserName: config.UserName
     };
 
     this.server = net.createServer((socket)=>{
       socket.on("data",(_data)=>{
-        const data = crypto.decode(_data.toString().trim(),config.key||"NONE");
+        const data = this.decode(_data.toString().trim(),config.key||"NONE");
         if(!data) return;
       
         if(data.event === "ADD_REQUEST"){
@@ -31,10 +37,50 @@ class P2P{
   run(){
     this.server.listen(this.config.ServerPort,()=>{
       console.log("P2Pネットワークが開始されました");
+      this.emit("start");
     });
   }
 
-  
+  DataSend(address,event,data){
+    if(!data||!event) throw new Error("オプションが不足しています");
+    try{
+      const client = net.connect({ host: address, port: this.config.ClientPort });
+      client.write(this.encode(JSON.stringify({
+        event: event,
+        client: {
+          version: package.version,
+          UserName: this.config.UserName,
+          ServerPort: this.config.ServerPort,
+          ClientPort: this.config.ClientPort
+        },
+        data: data,
+        time: new Date()
+      }),this.config.key));
+    }catch{
+      console.log("Send Failed");
+    }
+    client.end();
+  }
+
+  encode(data,key){
+    if(!data||!key) throw new Error("オプションが不足しています");
+    try{
+      const cipher = crypto.createCipher("aes-256-cfb",crypto.createHash("sha256").update(key).digest("hex"));
+      return cipher.update(data,"utf8","hex")+cipher.final("hex");
+    }catch(error){
+      return null;
+    }
+  }
+
+  decode(data,key){
+    if(!data||!key) throw new Error("オプションが不足してしていま");
+    try{
+      const decipher = crypto.createDecipher("aes-256-cfb",crypto.createHash("sha256").update(key).digest("hex"));
+      return JSON.parse(decipher.update(data,"hex","utf8")+decipher.final("utf8"));
+    }catch(error){
+      return null;
+    }
+  }
 }
 
 module.exports = P2P;
